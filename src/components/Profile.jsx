edfdc0cfb816add622eaf7cac085bb994c9e8099
Cusatelli <einarvandevelde@gmail.com>
2022-01-28 11:29:50 +0100

    feat: auth guard from profile & translate, redirect to login.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
-export default function Profile() {
+export default function Profile({ auth }) {
+    if (!auth) { return <Navigate to="/login" />; }
+    
     return (
         <div className="page profile">
             <h1>Profile Page</h1>
--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
-export default function Translate() {
+export default function Translate({ auth }) {
+    if (!auth) { return <Navigate to="/login" />; }
+
     return (
         <div className="page translate">
             <h1>Translate Page</h1>
